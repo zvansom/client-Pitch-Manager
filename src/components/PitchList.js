b0dc9652d5a10bba0384aeb6b3fa84204ca3868f
Zach VanSomeren <zachvansomeren@Zachs-MacBook-Pro.local>
2018-10-07 11:57:46 -0700

    Successful GraphQL query!

--- src/components/PitchList.js
+++ src/components/PitchList.js
@@ -1,13 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
 
-export default class PitchList extends Component {
+const getPitchesQuery = gql`
+  {
+    pitches {
+      title
+      id
+    }
+  }
+`;
+
+class PitchList extends Component {
+  displayPitches() {
+    const { data } = this.props;
+    if(data.loading) {
+      return ( <div>Loading books...</div>);
+    } else {
+      return data.pitches.map(pitch => {
+        return(
+          <li key={ pitch.id }>{pitch.title}</li>
+        );
+      });
+    }
+  }
   render() {
+    console.log(this.props);
     return (
       <div>
         <ul id="pitch-list">
-          <li>Pitch Title</li>
+          { this.displayPitches() }
         </ul>
       </div>
     )
   }
 }
+
+export default graphql(getPitchesQuery)(PitchList)
