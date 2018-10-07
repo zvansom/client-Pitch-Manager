8f3b644bc16cdc6da5ff801d79a3b641988b4111
Zach VanSomeren <zachvansomeren@Zachs-MacBook-Pro.local>
2018-10-07 10:49:24 -0700

    Initial Pitch Index component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
+
+import PitchList from './components/PitchList';
 require('dotenv').config();
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <div id="main">
+        <h1>Test</h1>
+        <PitchList />
       </div>
     );
   }
---
+++ src/components/PitchList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class PitchList extends Component {
+  render() {
+    return (
+      <div>
+        <ul id="pitch-list">
+          <li>Pitch Title</li>
+        </ul>
+      </div>
+    )
+  }
+}
