7740940eb0c1aa09c7d44bd98ed077f104b81909
Zach VanSomeren <zvansomeren@gmail.com>
2018-10-07 11:32:31 -0700

    Initial apollo set up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
 import PitchList from './components/PitchList';
 require('dotenv').config();
 
+// ApolloClient config
+const client = new ApolloClient({
+  uri: 'http://localhost:7777/graphql',
+});
+
 class App extends Component {
   render() {
     return (
-      <div id="main">
-        <h1>Test</h1>
-        <PitchList />
-      </div>
+      <ApolloProvider client={client} >
+        <div id="main">
+          <h1>Test</h1>
+          <PitchList />
+        </div>
+      </ApolloProvider>
     );
   }
 }
