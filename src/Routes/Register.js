ce9ac260c6967576a04e49319d0dbb67867b3296
Zach VanSomeren <zvansomeren@gmail.com>
2018-10-07 16:17:22 -0700

    Register form for tags corrected

--- src/Routes/Register.js
+++ src/Routes/Register.js
@@ -17,28 +17,28 @@ export default class Register extends Component {
     return (
       <form onSubmit={this.submitForm}>
         <h2>Register</h2>
-        <label hrmlfor="name">Name</label>
+        <label htmlFor="name">Name</label>
         <input 
           type="text" 
           id="name" 
           onChange={ e => { this.setState({ name: e.target.value }) } }
           value={this.state.name}
         />
-        <label htmlfor="email">Email</label>
+        <label htmlFor="email">Email</label>
         <input 
           type="text" 
           id="email" 
           onChange={ e => { this.setState({ email: e.target.value }) } }
           value={this.state.email}
         />
-        <label htmlfor="password">Password</label>
+        <label htmlFor="password">Password</label>
         <input 
           type="password" 
           id="password" 
           onChange={ e => { this.setState({ password: e.target.value }) } }
           value={this.state.password}
         />
-        <label htmlfor="password-confirm">Confirm Password</label>
+        <label htmlFor="password-confirm">Confirm Password</label>
         <input 
           type="password" 
           id="password-confirm" 
