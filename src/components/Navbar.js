51a64a8e39f4b8ef1e3139599fc5be453470ff0e
Zach VanSomeren <zachvansomeren@Zachs-MBP.local.tld>
2018-10-08 08:13:53 -0700

    Initial navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom';
 
+import "../Navbar.css";
+
 export default class Navbar extends Component {
   render() {
     return (
-      <nav>
+      <nav className="nav">
         <div className="nav__section nav__section--pages">
-          <NavLink activeClassName="active" exact to="/">Home</NavLink>
+          <NavLink 
+            className="nav__link" 
+            activeClassName="nav__link--active" 
+            exact to="/"
+          >
+            Home
+          </NavLink>
         </div>
         <div className="nav__section nav__section--user">
-          <NavLink activeClassName="active" to="/register">Register</NavLink>
-          <NavLink activeClassName="active" to="/login">Login</NavLink>
+          <NavLink 
+            className="nav__link" 
+            activeClassName="nav__link--active" 
+            to="/register"
+          >
+            Register
+          </NavLink>
+          <NavLink 
+            className="nav__link" 
+            activeClassName="nav__link--active" 
+            to="/login"
+          >
+            Login
+          </NavLink>
         </div>
       </nav>
     )
