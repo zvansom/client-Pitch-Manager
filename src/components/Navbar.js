878dff1b1e1a6dbe00a81e9ad208b80dca16d9a6
Zach VanSomeren <zachvansomeren@Zachs-MacBook-Pro.local>
2018-10-08 15:26:33 -0700

    Logout flow added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,40 @@ import { NavLink } from 'react-router-dom';
 import "../styles/Navbar.css";
 
 export default class Navbar extends Component {
+  handleLogout = e => {
+    console.log('logging out...');
+    e.preventDefault();
+    localStorage.removeItem('mernToken');
+    this.props.updateUser();
+  }
+
+  userNav() {
+    if(!this.props.user){
+      return(
+        <div className="nav__section nav__section--user">        
+          <NavLink 
+            className="nav__link" 
+            activeClassName="nav__link--active" 
+            to="/register">
+            Register
+          </NavLink>
+          <NavLink 
+            className="nav__link" 
+            activeClassName="nav__link--active" 
+            to="/login">
+            Login
+          </NavLink>
+        </div>        
+      )
+    } else {
+      return(
+        <button className="nav__link" onClick={this.handleLogout}>
+          Logout
+        </button>
+      )
+    }
+  }
+
   render() {
     return (
       <nav className="nav">
@@ -11,27 +45,11 @@ export default class Navbar extends Component {
           <NavLink 
             className="nav__link" 
             activeClassName="nav__link--active" 
-            exact to="/"
-          >
+            exact to="/">
             Home
           </NavLink>
         </div>
-        <div className="nav__section nav__section--user">
-          <NavLink 
-            className="nav__link" 
-            activeClassName="nav__link--active" 
-            to="/register"
-          >
-            Register
-          </NavLink>
-          <NavLink 
-            className="nav__link" 
-            activeClassName="nav__link--active" 
-            to="/login"
-          >
-            Login
-          </NavLink>
-        </div>
+        {this.userNav()}
       </nav>
     )
   }
