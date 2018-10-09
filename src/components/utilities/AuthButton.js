43d08ad533090940da2f75b4276f9aa30b835765
Zach VanSomeren <zachvansomeren@Zachs-MacBook-Pro.local>
2018-10-09 10:28:08 -0700

    Added user dashboard links.  Auth Button fuctional

--- src/components/utilities/AuthButton.js
+++ src/components/utilities/AuthButton.js
@@ -4,8 +4,23 @@ import {
   NavLink,
 } from "react-router-dom";
 
-const AuthButton = props => {
-  return null
-}
-
-export default AuthButton;
+export const AuthButton = withRouter(({ history, logout, authenticated }) => {
+  return (  
+    authenticated ? (
+      <button
+        className="nav__link"
+        onClick={() => { logout() }}
+      >
+        Sign out
+      </button>
+    ) : (
+      <NavLink
+        to='/login'
+        className="nav__link" 
+        activeClassName="nav__link--active"
+      >
+        Login
+      </NavLink>
+    )
+  );
+});
