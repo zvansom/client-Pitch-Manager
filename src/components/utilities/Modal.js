6c1b09d94eaf590515e9e4e2c80911e0bbe345d1
Zach VanSomeren <zvansomeren@gmail.com>
2018-10-11 11:48:16 -0700

    Portal and Modal components created. Styled components added

---
+++ src/components/utilities/Modal.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+import styled from 'styled-components';
+import Portal from './Portal';
+import Icon from '../utilities/Icon';
+
+export default class Modal extends Component {
+  render() {
+    const { children, toggle, on } = this.props;
+    return (
+      <Portal>
+        {on &&
+          <ModalWrapper>
+            <ModalCard>
+              <CloseButton onClick={toggle}><Icon name="close" /></CloseButton>
+              <div>{children}</div>
+            </ModalCard>
+            <Background onClick={toggle}/>
+          </ModalWrapper>
+        }
+      </Portal>
+    )
+  }
+}
+
+const ModalWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;  
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalCard = styled.div`
+  position: relative;
+  background: white;
+  border-radius: 5px;
+  padding: 15px;
+  min-width: 320px;
+  box-shadow: 2px 2px 10px rgba(0,0,0,0.3);
+  z-index: 10;
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  top: 0;
+  right: 0;
+  border: none;
+  background: transparent;
+  padding: 10px;
+`;
+
+const Background = styled.div`
+  position: absolute;
+  width: 100%;
+  top: 0;
+  left: 0;
+  height: 100%;
+  background: black;
+  opacity: 0.3;
+`;
