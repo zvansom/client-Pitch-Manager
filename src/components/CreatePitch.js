02ac0f10d9ecd37062f66602edfba1c3d53eed91
Zach VanSomeren <zvansomeren@gmail.com>
2018-11-02 10:06:08 -0700

    Refactored mutation into component

--- src/components/CreatePitch.js
+++ src/components/CreatePitch.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
-import { graphql, Query } from 'react-apollo';
+import { graphql, Query, Mutation } from 'react-apollo';
 import PropTypes from 'prop-types';
 
 import { 
@@ -27,75 +27,69 @@ class CreatePitch extends Component {
     redirect: false,
   };
 
-  submitForm = e => {
-    e.preventDefault();
-    const { title, description, client } = this.state;
-    const { user } = this.props;
-    this.props.addPitchMutation({
-      variables: {
-        user: user.id,
-        title,
-        description,
-        client,
-        status: client ? 'In Review' : 'Not Pitched',
-      },
-    }).then(({ data }) => {
-      console.log(data);
-      // TODO: resync cache with server
-      this.setState({ redirect: true });
-    }).catch(error => {
-      console.error(error);
-    });
-
+  redirect() {
     this.setState({
       title: '',
       description: '',
-      client: '',
-    });
+      client: null,
+      redirect: true,
+    })
   }
-
+  
   render() {
-    if(this.state.redirect) { return <Redirect to="/pitches" /> }
+    const { redirect, ...formData } = this.state;
+    if(redirect) { return <Redirect to="/pitches" /> }
     const { id } = this.props.user;
+    formData.user = id;
     return (
-      <form className="form" onSubmit={this.submitForm}>
-        <div className="field">
-          <label>Pitch title:</label>
-          <input 
-            type="text" 
-            onChange={ e => this.setState({ title: e.target.value })}
-            value={this.state.title} />
-        </div>
-        <div className="field">
-          <label>Description:</label>
-          <textarea 
-            onChange={ e => this.setState({ description: e.target.value })}
-            value={this.state.description}>
-          </textarea>
-        </div>
-        <div className="field">
-          <label>Client:</label>
-          <select onChange={ e => this.setState({ client: e.target.value })}>
-            <option>Select client</option>
-            <Query query={getClientsQuery} variables={{id}}>
-              {({ loading, error, data }) => {
-                if(loading) return <option disabled>Loading Clients...</option>;
-                if(error) return <option>Oops.  Something went wrong.</option>;
-                if(data.user.clients.length){
-                  return data.user.clients.map(client => {
-                    return(
-                      <option key={client.id} value={client.id}>{client.name}</option>
-                    );
-                  });
-                } else {
-                  return <option disabled>You don't have any clients</option>
-                }
-              }}
-            </Query>
-          </select> 
-        </div>
-        <input type="submit" value="Save Pitch" />
-      </form>
+      <Mutation mutation={addPitchMutation} variables={formData}>
+      {(createPitch, { loading, error }) => (
+        <form className="form" onSubmit={ async e => {
+          e.preventDefault();
+          const res = await createPitch();
+          console.log(res);
+          this.redirect();
+          }
+        }>
+          <div className="field">
+            <label>Pitch title:</label>
+            <input 
+              type="text" 
+              onChange={ e => this.setState({ title: e.target.value })}
+              value={this.state.title} />
+          </div>
+          <div className="field">
+            <label>Description:</label>
+            <textarea 
+              onChange={ e => this.setState({ description: e.target.value })}
+              value={this.state.description}>
+            </textarea>
+          </div>
+          <div className="field">
+            <label>Client:</label>
+            <select onChange={ e => this.setState({ client: e.target.value })}>
+              <option>Select client</option>
+              <Query query={getClientsQuery} variables={{id}}>
+                {({ loading, error, data }) => {
+                  if(loading) return <option disabled>Loading Clients...</option>;
+                  if(error) return <option>Oops.  Something went wrong.</option>;
+                  if(data.user.clients.length){
+                    return data.user.clients.map(client => {
+                      return(
+                        <option key={client.id} value={client.id}>{client.name}</option>
+                      );
+                    });
+                  } else {
+                    return <option disabled>You don't have any clients</option>
+                  }
+                }}
+              </Query>
+            </select> 
+          </div>
+          <input type="submit" value="Save Pitch" />
+        </form>
+      )}
+      </Mutation>
     )
   }
 }
